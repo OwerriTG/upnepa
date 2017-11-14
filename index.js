2faad685b92da5dd235b21afa2537c4321595a57
George Mario <c.georgeiwu@gmail.com>
2017-11-14 14:42:56 +0100

    initial

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
 
     // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "SETUP"
+    let VERIFY_TOKEN = "UPNEPA"
 
     // Parse the query params
     let mode = req.query['hub.mode'];
