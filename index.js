4dc0c13265daf8e8edbb2443942ff3f592cc835c
George Mario <c.georgeiwu@gmail.com>
2017-11-15 11:57:52 +0100

    Testing

--- index.js
+++ index.js
@@ -67,3 +67,36 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+// Adds support for GET requests to our webhook
+app.get('/', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "UPNEPA"
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        console.log('mode')
+        console.log(mode)
+        console.log('token')
+        console.log(token)
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
