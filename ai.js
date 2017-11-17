d5a3c23206157a9bc7ba872521fb1cf3e3ad6ac6
George Mario <c.georgeiwu@gmail.com>
2017-11-17 18:04:28 +0100

    building ai

--- ai.js
+++ ai.js
@@ -5,27 +5,27 @@ const
 function save(data){
 
     console.log('saving')
-    cache.setKey(data, new Date().toTimeString().substr(0, 5));
-    return 'Thank you upnepa at' + data + ' by ' + cache.getKey(data)
+    cache.setKey(data, { time: new Date().toTimeString().substr(0, 5)} );
+    return 'Thank you upnepa at' + data + ' by ' + cache.getKey(data).time
 }
 
 function find(data){
 
-    // Check if the message contains help
-    if (data.indexOf("help") !== -1) {
-        return 'Thanks for messaging us. \n' +
-            '\n' +
-            'To indicate light please use: \n' +
-            'upnepa [place] e.g upnapa worldbank \n' +
-            '\n' +
-            'To ask about light please use: \n' +
-            '[place] e.g worldbank'
-    }
-
-    if (cache.get(data)) {
-
-        return 'There was light at ' + data + ' by ' + cache.get(data)
-    }
+    // // Check if the message contains help
+    // if (data.indexOf("help") !== -1) {
+    //     return 'Thanks for messaging us. \n' +
+    //         '\n' +
+    //         'To indicate light please use: \n' +
+    //         'upnepa [place] e.g upnapa worldbank \n' +
+    //         '\n' +
+    //         'To ask about light please use: \n' +
+    //         '[place] e.g worldbank'
+    // }
+    //
+    // if (cache.getKey(data)) {
+    //
+    //     return 'There was light at ' + data + ' by ' + cache.get(data)
+    // }
 
     return 'Cant find ' + data + ' entry'
 }
